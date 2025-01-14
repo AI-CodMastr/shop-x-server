892a213e011949560e3ff1571695c382e26bf8ac
Farhan Aziz <mraziz459@gmail.com>
2025-01-14 23:08:21 +0500

    server has been done

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import data from './data/data.json' assert { type: 'json' };
+
+const app = express();
+const PORT = 4000;
+
+// Routes
+
+app.get('/categories', (req, res) => {
+  res.status(200).json(data.categories);
+});
+
+app.get('/products', (req, res) => {
+  res.status(200).json(data.products);
+});
+
+app.get('/saleProducts', (req, res) => {
+  res.status(200).json(data.saleProducts);
+});
+
+app.get('/notification', (req, res) => {
+  res.status(200).json(data.notifications);
+});
+
+app.get('/cartItems', (req, res) => {
+  res.status(200).json(data.cart);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
